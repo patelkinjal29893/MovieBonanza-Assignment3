59974ab3a1606b80bab62d419a3896d91a5667bc
KinjalPatel <patelkinjal298@hotmail.com>
2017-02-22 00:13:41 -0500

    Added New OrderForm and Add Some Controls in it

---
+++ MovieBonanza-Assignment3/OrderForm.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace MovieBonanza_Assignment3
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrderForm));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StreamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.CancelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.MovieSelectedGroupBox = new System.Windows.Forms.GroupBox();
+            this.YourOrderGroupBox = new System.Windows.Forms.GroupBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CategoryLabel = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.CategoryTextBox = new System.Windows.Forms.TextBox();
+            this.StreamButton = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.CostLabel = new System.Windows.Forms.Label();
+            this.SubTotalLabel = new System.Windows.Forms.Label();
+            this.SalesTaxLabel = new System.Windows.Forms.Label();
+            this.GrandTotalLabel = new System.Windows.Forms.Label();
+            this.CostTextBox = new System.Windows.Forms.TextBox();
+            this.SubTotalTextBox = new System.Windows.Forms.TextBox();
+            this.GrandTotalTextBox = new System.Windows.Forms.TextBox();
+            this.SalesTaxTextBox = new System.Windows.Forms.TextBox();
+            this.AdditionalChargeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.MovieSelectedGroupBox.SuspendLayout();
+            this.YourOrderGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(466, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.StreamToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.CancelToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("printToolStripMenuItem.Image")));
+            this.printToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.printToolStripMenuItem.Text = "&Print";
+            // 
+            // StreamToolStripMenuItem
+            // 
+            this.StreamToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("StreamToolStripMenuItem.Image")));
+            this.StreamToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.StreamToolStripMenuItem.Name = "StreamToolStripMenuItem";
+            this.StreamToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.StreamToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.StreamToolStripMenuItem.Text = "Strea&m";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(148, 6);
+            // 
+            // CancelToolStripMenuItem
+            // 
+            this.CancelToolStripMenuItem.Name = "CancelToolStripMenuItem";
+            this.CancelToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.CancelToolStripMenuItem.Size = new System.Drawing.Size(151, 22);
+            this.CancelToolStripMenuItem.Text = "Canc&el";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(334, 328);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(50, 23);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // MovieSelectedGroupBox
+            // 
+            this.MovieSelectedGroupBox.Controls.Add(this.PictureBox);
+            this.MovieSelectedGroupBox.Controls.Add(this.CategoryTextBox);
+            this.MovieSelectedGroupBox.Controls.Add(this.TitleTextBox);
+            this.MovieSelectedGroupBox.Controls.Add(this.CategoryLabel);
+            this.MovieSelectedGroupBox.Controls.Add(this.TitleLabel);
+            this.MovieSelectedGroupBox.Location = new System.Drawing.Point(13, 37);
+            this.MovieSelectedGroupBox.Name = "MovieSelectedGroupBox";
+            this.MovieSelectedGroupBox.Size = new System.Drawing.Size(198, 314);
+            this.MovieSelectedGroupBox.TabIndex = 2;
+            this.MovieSelectedGroupBox.TabStop = false;
+            this.MovieSelectedGroupBox.Text = "Movie Selected";
+            // 
+            // YourOrderGroupBox
+            // 
+            this.YourOrderGroupBox.Controls.Add(this.AdditionalChargeTextBox);
+            this.YourOrderGroupBox.Controls.Add(this.SalesTaxTextBox);
+            this.YourOrderGroupBox.Controls.Add(this.GrandTotalTextBox);
+            this.YourOrderGroupBox.Controls.Add(this.SubTotalTextBox);
+            this.YourOrderGroupBox.Controls.Add(this.CostTextBox);
+            this.YourOrderGroupBox.Controls.Add(this.GrandTotalLabel);
+            this.YourOrderGroupBox.Controls.Add(this.SalesTaxLabel);
+            this.YourOrderGroupBox.Controls.Add(this.SubTotalLabel);
+            this.YourOrderGroupBox.Controls.Add(this.label1);
+            this.YourOrderGroupBox.Controls.Add(this.CostLabel);
+            this.YourOrderGroupBox.Location = new System.Drawing.Point(246, 37);
+            this.YourOrderGroupBox.Name = "YourOrderGroupBox";
+            this.YourOrderGroupBox.Size = new System.Drawing.Size(208, 263);
+            this.YourOrderGroupBox.TabIndex = 3;
+            this.YourOrderGroupBox.TabStop = false;
+            this.YourOrderGroupBox.Text = "Your Order";
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(7, 20);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(27, 13);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Title";
+            // 
+            // CategoryLabel
+            // 
+            this.CategoryLabel.AutoSize = true;
+            this.CategoryLabel.Location = new System.Drawing.Point(7, 63);
+            this.CategoryLabel.Name = "CategoryLabel";
+            this.CategoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.CategoryLabel.TabIndex = 1;
+            this.CategoryLabel.Text = "Category";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Location = new System.Drawing.Point(10, 37);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.ReadOnly = true;
+            this.TitleTextBox.Size = new System.Drawing.Size(172, 20);
+            this.TitleTextBox.TabIndex = 2;
+            // 
+            // CategoryTextBox
+            // 
+            this.CategoryTextBox.Location = new System.Drawing.Point(10, 79);
+            this.CategoryTextBox.Name = "CategoryTextBox";
+            this.CategoryTextBox.ReadOnly = true;
+            this.CategoryTextBox.Size = new System.Drawing.Size(172, 20);
+            this.CategoryTextBox.TabIndex = 2;
+            // 
+            // StreamButton
+            // 
+            this.StreamButton.Location = new System.Drawing.Point(399, 328);
+            this.StreamButton.Name = "StreamButton";
+            this.StreamButton.Size = new System.Drawing.Size(55, 23);
+            this.StreamButton.TabIndex = 1;
+            this.StreamButton.Text = "Stream";
+            this.StreamButton.UseVisualStyleBackColor = true;
+            this.StreamButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(10, 116);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(172, 181);
+            this.PictureBox.TabIndex = 3;
+            this.PictureBox.TabStop = false;
+            // 
+            // CostLabel
+            // 
+            this.CostLabel.AutoSize = true;
+            this.CostLabel.Location = new System.Drawing.Point(61, 30);
+            this.CostLabel.Name = "CostLabel";
+            this.CostLabel.Size = new System.Drawing.Size(28, 13);
+            this.CostLabel.TabIndex = 0;
+            this.CostLabel.Text = "Cost";
+            // 
+            // SubTotalLabel
+            // 
+            this.SubTotalLabel.AutoSize = true;
+            this.SubTotalLabel.Location = new System.Drawing.Point(36, 100);
+            this.SubTotalLabel.Name = "SubTotalLabel";
+            this.SubTotalLabel.Size = new System.Drawing.Size(53, 13);
+            this.SubTotalLabel.TabIndex = 0;
+            this.SubTotalLabel.Text = "Sub Total";
+            // 
+            // SalesTaxLabel
+            // 
+            this.SalesTaxLabel.AutoSize = true;
+            this.SalesTaxLabel.Location = new System.Drawing.Point(9, 134);
+            this.SalesTaxLabel.Name = "SalesTaxLabel";
+            this.SalesTaxLabel.Size = new System.Drawing.Size(80, 13);
+            this.SalesTaxLabel.TabIndex = 0;
+            this.SalesTaxLabel.Text = "Sales Tax(13%)";
+            // 
+            // GrandTotalLabel
+            // 
+            this.GrandTotalLabel.AutoSize = true;
+            this.GrandTotalLabel.Location = new System.Drawing.Point(26, 166);
+            this.GrandTotalLabel.Name = "GrandTotalLabel";
+            this.GrandTotalLabel.Size = new System.Drawing.Size(63, 13);
+            this.GrandTotalLabel.TabIndex = 0;
+            this.GrandTotalLabel.Text = "Grand Total";
+            // 
+            // CostTextBox
+            // 
+            this.CostTextBox.Location = new System.Drawing.Point(96, 30);
+            this.CostTextBox.Name = "CostTextBox";
+            this.CostTextBox.ReadOnly = true;
+            this.CostTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CostTextBox.TabIndex = 1;
+            // 
+            // SubTotalTextBox
+            // 
+            this.SubTotalTextBox.Location = new System.Drawing.Point(96, 97);
+            this.SubTotalTextBox.Name = "SubTotalTextBox";
+            this.SubTotalTextBox.ReadOnly = true;
+            this.SubTotalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SubTotalTextBox.TabIndex = 1;
+            // 
+            // GrandTotalTextBox
+            // 
+            this.GrandTotalTextBox.Location = new System.Drawing.Point(96, 163);
+            this.GrandTotalTextBox.Name = "GrandTotalTextBox";
+            this.GrandTotalTextBox.ReadOnly = true;
+            this.GrandTotalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.GrandTotalTextBox.TabIndex = 1;
+            // 
+            // SalesTaxTextBox
+            // 
+            this.SalesTaxTextBox.Location = new System.Drawing.Point(96, 131);
+            this.SalesTaxTextBox.Name = "SalesTaxTextBox";
+            this.SalesTaxTextBox.ReadOnly = true;
+            this.SalesTaxTextBox.Size = new System.Drawing.Size(100, 20);
+            this.SalesTaxTextBox.TabIndex = 1;
+            // 
+            // AdditionalChargeTextBox
+            // 
+            this.AdditionalChargeTextBox.HideSelection = false;
+            this.AdditionalChargeTextBox.Location = new System.Drawing.Point(96, 64);
+            this.AdditionalChargeTextBox.Name = "AdditionalChargeTextBox";
+            this.AdditionalChargeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AdditionalChargeTextBox.TabIndex = 2;
+            this.AdditionalChargeTextBox.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Visible = false;
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 364);
+            this.Controls.Add(this.YourOrderGroupBox);
+            this.Controls.Add(this.MovieSelectedGroupBox);
+            this.Controls.Add(this.StreamButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "OrderForm";
+            this.Text = "OrderForm";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.MovieSelectedGroupBox.ResumeLayout(false);
+            this.MovieSelectedGroupBox.PerformLayout();
+            this.YourOrderGroupBox.ResumeLayout(false);
+            this.YourOrderGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StreamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem CancelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.GroupBox MovieSelectedGroupBox;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.TextBox CategoryTextBox;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Label CategoryLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.GroupBox YourOrderGroupBox;
+        private System.Windows.Forms.TextBox SalesTaxTextBox;
+        private System.Windows.Forms.TextBox GrandTotalTextBox;
+        private System.Windows.Forms.TextBox SubTotalTextBox;
+        private System.Windows.Forms.TextBox CostTextBox;
+        private System.Windows.Forms.Label GrandTotalLabel;
+        private System.Windows.Forms.Label SalesTaxLabel;
+        private System.Windows.Forms.Label SubTotalLabel;
+        private System.Windows.Forms.Label CostLabel;
+        private System.Windows.Forms.Button StreamButton;
+        private System.Windows.Forms.TextBox AdditionalChargeTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- MovieBonanza-Assignment3/SelectionForm.Designer.cs
+++ MovieBonanza-Assignment3/SelectionForm.Designer.cs
@@ -184,12 +184,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(655, 345);
+            this.ControlBox = false;
             this.Controls.Add(this.NextButton);
             this.Controls.Add(this.YourSelectionGroupBox);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.CurrentMoviesLabel);
             this.Controls.Add(this.HeadingLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "SelectionForm";
+            this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Movie Bonanza - Your Online Streaming Service";
             this.YourSelectionGroupBox.ResumeLayout(false);
